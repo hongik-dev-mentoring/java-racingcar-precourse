151ef9e5cf75d623d1a21aece486cda6e73fba79
Junek3 <rlarudrms28@gmail.com>
2023-01-14 04:33:15 +0900

    refactor: try-catch 블록에서 method 분리하기
    indent 제한을 지키기 위해 try-catch 블록의 코드들을 method로 분리하기로 함.
    method들이 하나의 기능만을 담당하도록 최대한 작세 분리해야함.
    method의 길이가 15라인을 넘어가지 않도록 구현해야 함.
    
    related to: #3

--- src/main/java/racingcar/InputProcess.java
+++ src/main/java/racingcar/InputProcess.java
@@ -16,21 +16,7 @@ public class InputProcess {
 		try {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 			String readLine = Console.readLine();
-			String[] splitedStrings = readLine.split(",");
-			carList = new ArrayList<>();
-			for (String s : splitedStrings) {
-				String carName = s.trim();
-				if (carName.length() > 5) {
-					StringBuilder sb = new StringBuilder();
-					String errorMessage = sb.append(errorTag)
-							.append("자동차 이름은 5자 이하만 가능합니다. ")
-							.append(carName)
-							.append("은 자동차 이름이 될 수 없습니다.")
-							.toString();
-					throw (new IllegalArgumentException(errorMessage));
-				}
-				carList.add(new Car(carName));
-			}
+			createCarListFromInputString(readLine);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			getCarList();
@@ -38,27 +24,47 @@ public class InputProcess {
 		return new ArrayList<>(carList);
 	}
 
+	private static void createCarListFromInputString(String input) {
+		String[] splitedStrings = input.split(",");
+		carList = new ArrayList<>();
+		for (String s : splitedStrings) {
+			String carName = s.trim();
+			checkStringLengthLimit(carName);
+			carList.add(new Car(carName));
+		}
+	}
+
+	private static void checkStringLengthLimit(String carName) {
+		if (carName.length() > 5) {
+			StringBuilder sb = new StringBuilder();
+			String errorMessage = sb.append(errorTag).append("자동차 이름은 5자 이하만 가능합니다. ")
+					.append(carName).append("은 자동차 이름이 될 수 없습니다.").toString();
+			throw (new IllegalArgumentException(errorMessage));
+		}
+	}
+
 	public static Integer getNumberOfMoves() {
 		try {
 			System.out.println("시도할 회수는 몇회인가요?");
 			String inputString = Console.readLine();
-			for (char c : inputString.toCharArray()) {
-				if (!Character.isDigit(c)) {
-					StringBuilder sb = new StringBuilder();
-					String errorMessage = sb.append(errorTag)
-							.append("이동횟수는 숫자만 입력할 수 있습니다. ")
-							.append(inputString)
-							.append("는 숫자가 아닙니다.")
-							.toString();
-					throw new IllegalArgumentException(errorMessage);
-				}
-			}
+			checkStringContainsOnlyNumber(inputString);
 			numberOfMoves = Integer.parseInt(inputString);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			getNumberOfMoves();
 		}
 		return numberOfMoves;
+	}
+
+	private static void checkStringContainsOnlyNumber(String inputString) {
+		for (char c : inputString.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				StringBuilder sb = new StringBuilder();
+				String errorMessage = sb.append(errorTag).append("이동횟수는 숫자만 입력할 수 있습니다.")
+						.append(inputString).append("는 숫자가 아닙니다.").toString();
+				throw new IllegalArgumentException(errorMessage);
+			}
+		}
 	}
 
 }

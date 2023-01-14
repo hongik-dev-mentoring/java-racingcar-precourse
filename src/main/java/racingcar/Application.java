ee5dd1cf69be6f0f8399fe07f4fe8bed19e3976c
Junek3 <rlarudrms28@gmail.com>
2023-01-14 09:24:21 +0900

    refactor: 실행결과 및 우승자 출력을 Race 클래스에서 담당하도록 변경하기
    기존에 main 메소드에 작성했던 실행결과와 우승자를 출력하는 부분을 Race 클래스의 method가 담당하도록 변경하였음.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,22 +11,9 @@ public class Application {
 		Integer moves = InputProcess.getNumberOfMoves();
 //		System.out.println("moves = " + moves);
 
-		Race race = new Race(cars);
-		System.out.println("실행결과");
-		for (int i = 0; i < moves; i++) {
-			race.proceed();
-			race.print();
-			System.out.println();
-		}
+		Race race = new Race(cars, moves);
+		race.start();
+		race.selectWinners();
 
-		List<Car> winners = race.selectWinners();
-		StringBuilder sb = new StringBuilder("최종 우승자 : ");
-		for (int i = 0; i < winners.size(); i++) {
-			sb.append(winners.get(i).getName());
-			if (i != winners.size() - 1) {
-				sb.append(", ");
-			}
-		}
-		System.out.println(sb);
 	}
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -7,13 +7,25 @@ public class Race {
 
 	private final List<Car> cars;
 
+	private final Integer moves;
+
 	private final List<Car> winners = new ArrayList<>();
 
-	public Race(List<Car> cars) {
+	public Race(List<Car> cars, Integer moves) {
 		this.cars = cars;
+		this.moves = moves;
 	}
 
-	public List<Car> selectWinners() {
+	public void start() {
+		System.out.println("실행결과");
+		for (int i = 0; i < moves; i++) {
+			this.proceed();
+			this.print();
+			System.out.println();
+		}
+	}
+
+	public void selectWinners() {
 		int maxPosition = -1;
 		for (Car car : cars) {
 			int position = car.getPosition();
@@ -27,7 +39,15 @@ public class Race {
 				winners.add(car);
 			}
 		}
-		return winners;
+
+		StringBuilder sb = new StringBuilder("최종 우승자 : ");
+		for (int i = 0; i < winners.size(); i++) {
+			sb.append(winners.get(i).getName());
+			if (i != winners.size() - 1) {
+				sb.append(", ");
+			}
+		}
+		System.out.println(sb);
 	}
 
 	public void proceed() {

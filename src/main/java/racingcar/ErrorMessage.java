c32ceac60091fa608fa5170e00a74a4a9c736707
Junek3 <rlarudrms28@gmail.com>
2023-01-15 20:21:05 +0900

    feat: Error message Enum으로 관리하기

---
+++ src/main/java/racingcar/ErrorMessage.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public enum ErrorMessage {
+	NOT_PROPER_NAME_LENGTH("자동차 이름은 1자 이상 5자 이하만 가능합니다."),
+	DUPLICATED_NAMES("중복된 자동차 이름이 존재합니다."),
+	INPUT_IS_NOT_NUMBER("이동횟수는 숫자만 입력할 수 있습니다."),
+	INPUT_IS_NOT_ALLOWED_UNDER_ZERO("이동횟수는 1이상의 숫자만 가능합니다."),
+	NOT_ENOUGH_CARS("경주에 참가하는 자동차가 한대 이상이어야 합니다.");
+//	NOT_ALLOWED_BLANK_OF_NAME("이름에 공백이 들어갈 수 없습니다.");
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "[ERROR] " + this.message;
+	}
+}

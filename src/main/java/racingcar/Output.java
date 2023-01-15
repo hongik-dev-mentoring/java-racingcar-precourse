5c3b857ad627b1ca30a6b83412a695d8fc5f528e
LTSGOD <lts0424@naver.com>
2023-01-15 12:25:41 +0900

    feat(Output): add printRound method

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,2 +1,14 @@
-package racingcar;public class Output {
+package racingcar;
+
+public class Output {
+    public static void printRound(Car c1){
+        System.out.print(c1.getName() +": ");
+        printBar(c1);
+    }
+    public static void printBar(Car c1){
+        for(int i = 0; i <c1.getPosition(); i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }

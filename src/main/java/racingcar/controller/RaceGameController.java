5dda0931478f92a5db4cf600009c9cb0a3c026f5
euichanhwang <pine6971@naver.com>
2023-01-15 17:16:13 +0900

    feat : RaceGameController 구조 설계
    ready() 메서드를 통해 경주 시작 전 준비하는 일을 합니다.
    run() 메서드를 통해 자동차 경주 게임을 진행합니다.
    announceWinner() 메서드를 통해 자동차 경주 게임의 승자를 발표합니다.

--- src/main/java/racingcar/controller/RaceGameController.java
+++ src/main/java/racingcar/controller/RaceGameController.java
@@ -1,5 +1,37 @@
 package racingcar.controller;
 
+import java.util.List;
+
+import racingcar.model.Cars;
+import racingcar.util.generator.NameGenerator;
+import racingcar.view.InputData;
+import racingcar.view.OutputData;
+
 public class RaceGameController {
-	
+	private Cars cars;
+	private int AttemptNumbers;
+
+	public void ready() {
+		cars = new Cars();
+		addCarNamesToCarList();
+	}
+
+	public void run() {
+
+	}
+
+	public void announceWinner() {
+
+	}
+
+	private void addCarNamesToCarList() {
+		try {
+			List<String> carNamesList = NameGenerator.generateCarNames(InputData.getCarNames());
+			cars.addCarNames(carNamesList);
+		} catch (IllegalArgumentException e) {
+			OutputData.printErrorMessage(e.getMessage()); // [ERROR] + 메시지 출력
+			addCarNamesToCarList();
+		}
+	}
+
 }
--- src/main/java/racingcar/view/OutputData.java
+++ src/main/java/racingcar/view/OutputData.java
@@ -1,4 +1,9 @@
 package racingcar.view;
 
 public class OutputData {
+	private static final String ERROR_MESSAGE = "[ERROR] ";
+
+	public static void printErrorMessage(String message) {
+		System.out.println(ERROR_MESSAGE + message);
+	}
 }

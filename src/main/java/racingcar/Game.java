179c3355544278daba878a641a1c64600b383ca1
LTSGOD <lts0424@naver.com>
2023-01-15 12:20:15 +0900

    refact(Game): delete ScoreBoard
    - Because of using sorted method remove all ScoreBoard method
    
    - add sorted method to maintain order of cars
    
    - add SortedCars property

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,13 +1,12 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Game {
     private ArrayList<Car> cars = new ArrayList<Car>();
+    private List<Car> SortedCars;
     private int TryNum;
-    private HashMap<String, Integer> scoreBoard;
     private int carNum;
 
     public Game(){
@@ -15,62 +14,51 @@ public class Game {
         TryNum = Input.ReadTryNum();
         setCars(StringInput);
         carNum = cars.size();
-        setScoreBoad();
 
-//        for(String i : scoreBoard.keySet()){
-//            System.out.println("key: " + i + " value: " + scoreBoard.get(i));
-//        } scoreboard 출력 테스트
-
+        System.out.println("실행 결과");
         Play();
+        for(Car c1: cars){
+            System.out.println("name: " + c1.getName() + " win : " + c1.getWinNum());
+        }
     }
 
     public void Play(){
         for(int i = 0; i <TryNum; i++){
             Round();
             SortCars();
-
+            Score();
         }
     }
 
     public void SortCars(){
-        Collections.sort(cars, (c1, c2) -> c2.getPosition() - c1.getPosition());
+        SortedCars = cars.stream().sorted(Comparator.comparing(Car::getPosition).reversed()).collect(Collectors.toList());
     }
 
     public void Score(){
-        int max = cars.get(0).getPosition();
-        updateValue(cars.get(0).getName(),cars.get(0).getPosition());
-        for(int i = 1; i <carNum; i++){
-            Car c1 = cars.get(i);
-            if(c1.getPosition() == max) updateValue(c1.getName(),max);
-        }
-    }
+        Car maxCar = SortedCars.get(0);
+        maxCar.increaseWinNum();
+        int max = maxCar.getPosition();
 
-    public void updateValue(String name, int value){
-        if(scoreBoard.containsKey(name) == false){
-            scoreBoard.put(name,value);
-            return;
+        for(int i = 1; i <carNum; i++){
+            Car c1 = SortedCars.get(i);
+            if(c1.getPosition() != max){
+                break;
+            }
+            c1.increaseWinNum();
         }
-        int tmp = scoreBoard.get(name);
-        scoreBoard.put(name, tmp + value);
-        return;
     }
 
     public void Round(){
         for(Car c1 : cars){
             c1.Race(TryNum);
+            Output.printRound(c1);
         }
+        System.out.println();
     }
 
     public void setCars(ArrayList<String> input){
         for(int i = 0; i <input.size(); i++){
             cars.add(new Car(input.get(i)));
-        }
-    }
-
-    public void setScoreBoad(){
-        scoreBoard = new HashMap<String, Integer>();
-        for(int i = 0; i < carNum; i++){
-            scoreBoard.put(cars.get(i).getName(), 0);
         }
     }
 }

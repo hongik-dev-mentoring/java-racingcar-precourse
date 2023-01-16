d5a28d6980f39601156ea11e2cc7d244c073b8ca
euichanhwang <pine6971@naver.com>
2023-01-16 12:41:55 +0900

    refactor : 유효성 검사 시점 변경
    유효성 검사 시점을 자동차 이름을 생성할 때가 아닌 Cars 일급 콜렉션에 추가할 때
    로 변경하였습니다.
    NameGenerator 클래스에서 검증 로직을 진행할 필요가 없다고 생각했습니다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -3,11 +3,15 @@ package racingcar.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import racingcar.util.validator.NameValidator;
+
 public class Cars {
 	private List<Car> carList = new ArrayList<>();
 
 	public void addCarNames(List<String> carNamesList) {
+		NameValidator.validateCarNames(carNamesList.toString());
 		for (String carName : carNamesList) {
+			NameValidator.validateCarName(carName);
 			this.carList.add(new Car(carName));
 		}
 	}
--- src/main/java/racingcar/util/generator/NameGenerator.java
+++ src/main/java/racingcar/util/generator/NameGenerator.java
@@ -3,17 +3,10 @@ package racingcar.util.generator;
 import java.util.Arrays;
 import java.util.List;
 
-import racingcar.util.validator.NameValidator;
-
 public class NameGenerator {
 	private static final String SEPARATE_BY_COMMA = ",";
 
 	public static List<String> generateCarNames(String carNames) {
-		NameValidator.validateCarNames(carNames);
-		List<String> carNamesList = Arrays.asList(carNames.split(SEPARATE_BY_COMMA));
-		for (String carName : carNamesList) {
-			NameValidator.validateCarName(carName);
-		}
-		return carNamesList;
+		return Arrays.asList(carNames.split(SEPARATE_BY_COMMA));
 	}
 }

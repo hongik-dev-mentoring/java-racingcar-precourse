b231c20d02bf271f2adbcb488d69ad25641ea279
Jinho <dfghcvb11@naver.com>
2023-01-19 17:45:55 +0900

    refactor: CarRacingController 인스턴스화 방지

--- src/main/java/racingcar/CarRacingController.java
+++ src/main/java/racingcar/CarRacingController.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public class CarRacingController {
 
+    private CarRacingController() {
+        throw new AssertionError();
+    }
+
     public static void initCarRacingGame() {
         Input input = new Input();
         List<String> carNamesList = input.getCarNamesList();
